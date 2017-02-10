08f4502aac1822e4f03f1a2abec18507f4cb5cec
lukas <you@example.com>
2017-02-10 10:46:20 -0800

    Added overall scripts for running, FD-calculating and plotting

---
+++ plotall.sh
@@ -0,0 +1,21 @@
+printf "\033[1;4;92mSTARTING shapesens                                                        \033[00m\n"
+cd shapesens
+./plotall.sh
+cd ..
+printf "\033[1;4;92mENDEN shapesens                                                        \033[00m\n"
+
+
+printf "\033[1;4;92mSTARTING machsens                                                        \033[00m\n"
+cd machsens
+./plotall.sh
+cd ..
+printf "\033[1;4;92mENDED machsens                                                        \033[00m\n"
+
+
+printf "\033[1;4;92mSTARTING alphasens                                                        \033[00m\n"
+cd alphasens
+./plotall.sh
+cd ..
+printf "\033[1;4;92mENDING alphasens                                                        \033[00m\n"
+
+
---
+++ runall.sh
@@ -0,0 +1,21 @@
+printf "\033[1;4;92mSTARTING shapesens                                                        \033[00m\n"
+cd shapesens
+./runall.sh
+cd ..
+printf "\033[1;4;92mENDEN shapesens                                                        \033[00m\n"
+
+
+printf "\033[1;4;92mSTARTING machsens                                                        \033[00m\n"
+cd machsens
+./runall.sh
+cd ..
+printf "\033[1;4;92mENDED machsens                                                        \033[00m\n"
+
+
+printf "\033[1;4;92mSTARTING alphasens                                                        \033[00m\n"
+cd alphasens
+./runall.sh
+cd ..
+printf "\033[1;4;92mENDING alphasens                                                        \033[00m\n"
+
+
