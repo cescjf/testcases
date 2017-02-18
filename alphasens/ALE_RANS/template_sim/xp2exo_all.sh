ed55b49a07e12f6204979b9f85fb677c8efbe4f7
scheuclu <scheuclu@gmail.com>
2017-02-18 12:55:14 -0800

    Added routines for paraview output of Sensitivity auxiliary quantities

---
+++ alphasens/ALE_RANS/template_sim/xp2exo_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+XP2EXO_EXECUTABLE=/home/pavery/bin/xp2exo
+
+$XP2EXO_EXECUTABLE ../mesh/naca.top \
+                   ./results/exo/all_quantities.exo \
+                   results/xpost/DensitySensitivity.xpost \
+                   results/xpost/MachSensitivity.xpost \
+                   results/xpost/PressureSensitivity.xpost \
+                   results/xpost/TemperatureSensitivity.xpost \
+                   results/xpost/TotalPressureSensitivity.xpost \
+                   results/xpost/VelocityNormSensitivity.xpost \
+                   results/xpost/VelocitySensitivity.xpost \
+                   results/xpost/DisplacementSensitivity.xpost
